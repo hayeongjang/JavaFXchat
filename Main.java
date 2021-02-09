8d6d98ccdb66a4a39395443d539bb82622a67e34
hayeongjang <77962884+hayeongjang@users.noreply.github.com>
2021-02-09 23:03:25 +0900

    chatting

--- Main.java
+++ Main.java
@@ -1,209 +1,160 @@
 package application;
 	
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Iterator;
+import java.util.Vector;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.geometry.Insets;
-import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
 
 
 
 public class Main extends Application {
 	
-	Socket socket;
+	// 다양한 클라이언트가 접속 했을 때 스레드를 효과적으로 관리 
 	
-	// 다양한 내용 주고 받았을 때 그런 메시지들을 출력하는 공간
-	TextArea textArea; 
+	// threadPool 이용, 다양한 클라이언트 접속 했을 때 스레드들 효과적으로 관리, 한정된 자원을 이용해 안정적으로 서버운영
+	public static ExecutorService threadPool;   
 	
-	// 클라이언트 프로그램 동작 메소드
-	public void startClient(String IP, int port) {
-		Thread thread = new Thread() { 
+	//접속한 클라이언트 관리
+	public static Vector<Client> clients = new Vector<Client>(); 
+	
+	ServerSocket serverSocket;
+	
+	// 서버를 구동시켜서 클라이언트의 연결을 기다리는 메소드
+	public void startServer(String IP , int port) {
+		try {
+			serverSocket = new ServerSocket();
+			
+			// 소켓 통신은 소켓에 대한 객체를 활성, 서버 역할 수행하는 컴퓨터가 특정한 클라이언트 접속 기다림
+			serverSocket.bind(new InetSocketAddress(IP, port));  
+		} catch (Exception e) {
+			e.printStackTrace();
+			if(!serverSocket.isClosed()) {
+				stopServer(); 
+			}
+			return;
+		}
+		// 클라이언트가 접속할 때까지 계속 기다리는 스레드
+		Runnable thread = new Runnable() {
+			@Override
 			public void run() {
-				try {
-					// 소켓 초기화
-					socket = new Socket(IP, port); 
-					
-					// 서버로 부터 메시지 전달 받음
-					receive();  
-					
-					} catch (Exception e) {
-					if(!socket.isClosed()) {
-						stopClient();
-						System.out.println("[서버 접속 실패]");
+				while(true) {
+					try {
+						// 새로운 클라이언트가 접속할 수 있게 만들어줌
+						Socket socket = serverSocket.accept();
 						
-						// 프로그램 자체 종료
-						Platform.exit();  
+						// 새롭게 접속한 클라이언트 추가
+						clients.add(new Client(socket));   
+						System.out.println("[클라이언트 접속]"
+							+ socket.getRemoteSocketAddress()
+							+ ": " + Thread.currentThread().getName());
+					} catch (Exception e) {
+						if(!serverSocket.isClosed()) {
+							stopServer();
+						}
+						break;
 					}
 				}
 			}
 		};
-		thread.start();
+		// threadPool 초기화
+		threadPool = Executors.newCachedThreadPool(); 
+		
+		// threadPool에 현재 클라이언트를 기다리는 스레드를 담을 수 있게 처리
+		threadPool.submit(thread);   
 	}
 	
-	// 클라이언트 프로그램 종료 메소드
-	public void stopClient() {
+	// 서버의 작동을 중지시키는 메소드
+	public void stopServer() {
 		try {
-			if(socket != null && !socket.isClosed()) {
-				socket.close();
+			// 현재 작동중인 모든 소켓 닫기
+			
+			// 모든 클라이언트에 개별적으로 접근
+			Iterator<Client> iterator = clients.iterator(); 
+			
+			// 하나씩 접속
+			while(iterator.hasNext()) {   
+				Client client = iterator.next();
+				client.socket.close();
+				iterator.remove();
+			}
+			// 서버 소켓 객체 닫기
+			if(serverSocket != null && ! serverSocket.isClosed()) { 
+				serverSocket.close();
+			}
+			// 스레드 풀 종료하기
+			// 자원 할당 해제
+			if(threadPool != null && ! threadPool.isShutdown()) { 
+				threadPool.shutdown();
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
-	// 서버로부터 메시지를 전달받는 메소드
-	public void receive() {
-		
-		// 무한 루프
-		while(true) { 
-		
-			try {
-				// 소켓에서 InputStream을 열어서 현재 서버로부터 어떠한 메시지를 전달받을 수 있도록 해줌
-				InputStream in = socket.getInputStream(); 
-				
-				byte[] buffer = new byte[512];
-				int length = in.read(buffer);
-				if(length == -1) throw new IOException();
-				String message = new String(buffer, 0, length, "UTF-8");
-				Platform.runLater(()->{
-					textArea.appendText(message);
-				});
-			} catch (Exception e) {
-				stopClient();
-				break;
-			}
-		}
-	}
-	
-	// 서버로 메시지를 전송하는 메소드
-	public void send(String message) {
-		Thread thread = new Thread() {
-			public void run() {
-				try {
-					OutputStream out = socket.getOutputStream();
-					byte[] buffer = message.getBytes("UTF-8");
-					out.write(buffer);
-					out.flush();  
-				} catch (Exception e) {
-					stopClient();
-				}
-			}
-		};
-		thread.start();
-	}
-	
-	// 실제로 프로그램을 동작시키는 메소드
+	// UI를 생성하고, 실질적으로 프로그램을 동작시키는 (사용자에게 보여지는) 메소드
 	@Override
 	public void start(Stage primaryStage) {
-		BorderPane root = new BorderPane();
+		// 레이아웃 생성
+		BorderPane root = new BorderPane(); 
 		root.setPadding(new Insets(5));
 		
-		HBox hbox = new HBox();
-		hbox.setSpacing(5);
+		// 긴 문장의 텍스트가 담기는 공간
+		TextArea textArea = new TextArea();  
 		
-		TextField userName = new TextField();
-		userName.setPrefWidth(150);
-		userName.setPromptText("닉네임 입력");
-		HBox.setHgrow(userName, Priority.ALWAYS);
+		// 수정불가, 어떠한 문장을 단순히 출력만 가능하게 함
+		textArea.setEditable(false);  
 		
-		TextField IPText = new TextField("127.0.0.1");
-		TextField portText = new TextField("1234");
-		portText.setPrefWidth(80);
+		textArea.setFont(new Font("나눔 고딕", 15));
+		root.setCenter(textArea); 
 		
-		hbox.getChildren().addAll(userName, IPText, portText);
-		root.setTop(hbox);
+		Button toggleButton = new Button("시작하기");
+		toggleButton.setMaxWidth(Double.MAX_VALUE);
+		BorderPane.setMargin(toggleButton, new Insets(1, 0, 0, 0));
+		root.setBottom(toggleButton);
 		
-		textArea = new TextArea();
-		textArea.setEditable(false); 
-		root.setCenter(textArea);
+		String IP = "127.0.0.1";  
+		int port = 1234;
 		
-		TextField input = new TextField();
-		input.setPrefWidth(Double.MAX_VALUE);
-		input.setDisable(true);
+		// 사용자가 토글버튼 눌렀을 때 이번트 발생
+		toggleButton.setOnAction(event -> {  
 		
-		input.setOnAction(event -> {
-			
-			// 서버로 어떠한 메시지를 전달할 수 있도록 함 (사용자 이름과 함께)
-			send(userName.getText() + " :" + input.getText() + "\n"); 
-			input.setText("");
-			input.requestFocus();
-		});
-		
-		Button sendButton = new Button("보내기");
-		sendButton.setDisable(true);
-		
-		sendButton.setOnAction(event -> {
-			send(userName.getText() + ": " + input.getText() + "\n"); 
-			input.setText("");
-			input.requestFocus();
-		});
-		
-		Button connectionButton = new Button("접속하기");
-		connectionButton.setOnAction(event -> {
-			if(connectionButton.getText().equals("접속하기")) {
-				int port = 1234;
-				try {
-					// 기본적으로 포트 번호는 1234, 사용자가 별도의 포트번호를 입력하면 그 포트로 접속이 이뤄지게 함
-					port = Integer.parseInt(portText.getText()); 
-				
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-				startClient(IPText.getText(), port);
-				Platform.runLater(() -> {
-					textArea.appendText("[ 채팅방 접속 ]\n");
+			if(toggleButton.getText().equals("시작하기")) {
+				startServer(IP, port);
+				Platform.runLater(()-> { 
+					String message = String.format("[서버 시작]\n",IP, port);
+					textArea.appendText(message);
+					toggleButton.setText("종료하기");
 					});
-				connectionButton.setText("종료하기");
-				input.setDisable(false); 
-				
-				// 사용자가 버튼을 눌러서 메시지 전송할 수 있도록 함
-				sendButton.setDisable(false);  
-				
-				input.requestFocus();
-			} else {
-				stopClient();
-				Platform.runLater(() -> {
-					textArea.appendText("[ 채팅방 퇴장 ]\n");
-				});
-				connectionButton.setText("접속하기");
-				
-				// 버튼 누를 수 없도록 함
-				input.setDisable(true);  
-				sendButton.setDisable(true);
-			}
+				} else {
+					stopServer();
+					Platform.runLater(()-> {
+						String message = String.format("[서버 종료]\n",IP, port);
+						textArea.appendText(message);
+						toggleButton.setText("시작하기");
+						});
+					
+				}
 		});
-		BorderPane pane = new BorderPane();
-		
-		// 접속하기 버튼
-		pane.setLeft(connectionButton); 
-		
-		// 사용자가 입력할 수 있도록
-		pane.setCenter(input); 
-		
-		// 보내기 버튼
-		pane.setRight(sendButton); 
-		
-		root.setBottom(pane);
 		Scene scene = new Scene(root, 400, 400);
-		primaryStage.setTitle("[ 채팅 클라이언트 ]");
+		primaryStage.setTitle("[채팅 서버]");
+		primaryStage.setOnCloseRequest(event -> stopServer());
 		primaryStage.setScene(scene);
-		primaryStage.setOnCloseRequest(event -> stopClient());
 		primaryStage.show();
-		
-		connectionButton.requestFocus();
-		
 	}
+		
 	// 프로그램의 진입점
 	public static void main(String[] args) {
 		launch(args);
